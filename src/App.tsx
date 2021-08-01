3dee8bfa9b2dac46ba776cf498ee9ec5ecc0d7da
Usman Ahmed <usmanahmed1100@gmail.com>
2021-08-01 19:16:50 +0500

    Small changes...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import {Typography} from "@material-ui/core"
+import { Typography } from "@material-ui/core";
 
 function App() {
 	return (
 		<>
-    <Typography variant="h1">
-		Hello Portfolio
-    </Typography>
+			<Typography variant="h1">Hello Portfolio</Typography>
+
+			<Typography variant="h3">
+				<i>Built with React, Material-UI, and TypeScript.</i>
+			</Typography>
+
+			<Typography variant="h5">
+				Actual portfolio coming soon...
+			</Typography>
+
+
+			
 		</>
 	);
 }
